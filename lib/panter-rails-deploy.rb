42e944d5b42cb41665c072bf8f9f6e2c05fceed5
Markus Koller <markus-koller@gmx.ch>
2015-06-01 17:51:03 +0300

    Don't require Rails gems when running in Capistrano

--- lib/panter-rails-deploy.rb
+++ lib/panter-rails-deploy.rb
@@ -1,6 +1,8 @@
-require 'unicorn-rails'
-require 'dotenv-rails'
-
 if defined? Capistrano
+  # We're running in Capistrano, only load tasks
   load File.expand_path('../capistrano/tasks/panter-rails-deploy.rake', __FILE__)
+else
+  # We're running in Rails, load supporting gems
+  require 'unicorn-rails'
+  require 'dotenv-rails'
 end

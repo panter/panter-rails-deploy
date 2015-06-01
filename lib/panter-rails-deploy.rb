e732f9097e326406932369bc8c7be6eba4b9c4b0
Markus Koller <markus-koller@gmx.ch>
2015-06-01 17:20:17 +0300

    Only load tasks when running in Capistrano

--- lib/panter-rails-deploy.rb
+++ lib/panter-rails-deploy.rb
@@ -1,1 +1,3 @@
-load File.expand_path('../capistrano/tasks/panter-rails-deploy.rake', __FILE__)
+if defined? Capistrano
+  load File.expand_path('../capistrano/tasks/panter-rails-deploy.rake', __FILE__)
+end
